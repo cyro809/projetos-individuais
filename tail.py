7f6572c69df5f3abf38f5b96db2e0eea4ce06b23
cyro809 <cyro809@gmail.com>
2015-01-18 17:33:22 -0200

    Refactor da tail.py para transformar em classe para os testes

--- tail.py
+++ tail.py
@@ -4,63 +4,70 @@
 import sys
 import time
 
+class Tail:
+    def __init__(self):
+        self.input_string = raw_input()
+        self.comando = self.input_string.split()[0]
+        self.num_de_linhas = 10
+        self.flag = None
+        self.argumentos = []
 
-input_string = raw_input()
-comando = input_string.split()[0]
+    def le_comando(self):
+        while self.comando != 'tail' or len(self.input_string.split()) == 1:
+            print u'Commando não reconhecido. Por favor, tente novamente'
 
-while comando != 'tail' or len(input_string.split()) == 1:
-    print u'Commando não reconhecido. Por favor, tente novamente'
+            self.input_string = raw_input()
 
-    input_string = raw_input()
+            self.comando = self.input_string.split()
 
-    comando = input_string.split()
+        self.argumentos = self.input_string.split()[1:]
 
-argumentos = input_string.split()[1:]
+        if len(self.argumentos) == 1:
+            nome_arquivo = self.argumentos[0]
+            arquivo = open(nome_arquivo, 'r')
 
-num_de_linhas = 10
-flag = None
+        elif len(self.argumentos) == 2:
+            nome_arquivo = self.argumentos[1]
+            arquivo = open(nome_arquivo, 'r')
+            self.flag = self.argumentos[0]
 
-if len(argumentos) == 1:
-    nome_arquivo = argumentos[0]
-    arquivo = open(nome_arquivo, 'r')
+            if self.flag != '-f':
+                print 'Uso errado da flag'
 
-elif len(argumentos) == 2:
-    nome_arquivo = argumentos[1]
-    arquivo = open(nome_arquivo, 'r')
-    flag = argumentos[0]
+        elif len(self.argumentos) == 3:
+            self.flag = self.argumentos[0]
 
-    if flag != '-f':
-        print 'Uso errado da flag'
+            if self.flag == '-n':
+                self.num_de_linhas = int(self.input_string.split()[2])
+                nome_arquivo = self.argumentos[2]
+            else:
+                print u'flag não reconhecida!'
+                exit()
 
-elif len(argumentos) == 3:
-    flag = argumentos[0]
+            arquivo = open(nome_arquivo, 'r')
 
-    if flag == '-n':
-        num_de_linhas = int(input_string.split()[2])
-        nome_arquivo = argumentos[2]
-    else:
-        print u'flag não reconhecida!'
-        exit()
-
-    arquivo = open(nome_arquivo, 'r')
-
-linhas = arquivo.readlines()
-total_de_linhas = len(linhas)
-linha_limite = total_de_linhas - num_de_linhas
-
-for i in range(linha_limite, total_de_linhas):
-    sys.stdout.write(linhas[i])
-
-total_de_linhas_atual = total_de_linhas
-if flag and flag == '-f':
-    while True:
-        total_de_linhas = total_de_linhas_atual
-        arquivo = open(nome_arquivo, 'r')
         linhas = arquivo.readlines()
-        total_de_linhas_atual = len(linhas)
+        total_de_linhas = len(linhas)
+        linha_limite = total_de_linhas - self.num_de_linhas
 
-        if total_de_linhas != total_de_linhas_atual:
-            ultima_linha = linhas[total_de_linhas_atual - 1]
-            print ultima_linha
-        else:
-            time.sleep(5)
+        for i in range(linha_limite, total_de_linhas):
+            sys.stdout.write(linhas[i])
+
+        total_de_linhas_atual = total_de_linhas
+        if self.flag and self.flag == '-f':
+            while True:
+                total_de_linhas = total_de_linhas_atual
+                arquivo = open(nome_arquivo, 'r')
+                linhas = arquivo.readlines()
+                total_de_linhas_atual = len(linhas)
+
+                if total_de_linhas != total_de_linhas_atual:
+                    ultima_linha = linhas[total_de_linhas_atual - 1]
+                    print ultima_linha
+                else:
+                    time.sleep(5)
+
+
+tail = Tail()
+
+tail.le_comando()
