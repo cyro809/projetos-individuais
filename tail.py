706c99ff80037a1e693f77aa26d327c037068c1c
cyro809 <cyro809@gmail.com>
2015-01-18 21:41:29 -0200

    Refactor do tail.py para ficar parecido com o head.py

--- tail.py
+++ tail.py
@@ -11,24 +11,29 @@ class Tail:
         self.num_de_linhas = 10
         self.flag = None
         self.argumentos = []
+        self.arquivo = None
+        self.nome_arquivo = None
+        self.linhas_a_imprimir = None
 
     def le_comando(self):
         while self.comando != 'tail' or len(self.input_string.split()) == 1:
-            print u'Commando não reconhecido. Por favor, tente novamente'
+            if self.comando != None:
+                print u'Commando não reconhecido. Por favor, tente novamente'
 
             self.input_string = raw_input()
 
             self.comando = self.input_string.split()
 
+        self.confere_argumentos()
+
+    def confere_argumentos(self):
         self.argumentos = self.input_string.split()[1:]
 
         if len(self.argumentos) == 1:
-            nome_arquivo = self.argumentos[0]
-            arquivo = open(nome_arquivo, 'r')
+            self.nome_arquivo = self.argumentos[0]
 
         elif len(self.argumentos) == 2:
-            nome_arquivo = self.argumentos[1]
-            arquivo = open(nome_arquivo, 'r')
+            self.nome_arquivo = self.argumentos[1]
             self.flag = self.argumentos[0]
 
             if self.flag != '-f':
@@ -39,33 +44,38 @@ class Tail:
 
             if self.flag == '-n':
                 self.num_de_linhas = int(self.input_string.split()[2])
-                nome_arquivo = self.argumentos[2]
+                self.nome_arquivo = self.argumentos[2]
             else:
                 print u'flag não reconhecida!'
                 exit()
 
-            arquivo = open(nome_arquivo, 'r')
+        self.arquivo = open(self.nome_arquivo, 'r')
 
-        linhas = arquivo.readlines()
+        self.imprimi_resultado()
+
+    def imprimi_resultado(self):
+        linhas = self.arquivo.readlines()
         total_de_linhas = len(linhas)
         linha_limite = total_de_linhas - self.num_de_linhas
 
-        for i in range(linha_limite, total_de_linhas):
-            sys.stdout.write(linhas[i])
+        self.linhas_a_imprimir = linhas[linha_limite:total_de_linhas]
+
+        for i in range(len(self.linhas_a_imprimir)):
+            sys.stdout.write(self.linhas_a_imprimir[i])
 
         total_de_linhas_atual = total_de_linhas
         if self.flag and self.flag == '-f':
             while True:
                 total_de_linhas = total_de_linhas_atual
-                arquivo = open(nome_arquivo, 'r')
-                linhas = arquivo.readlines()
+                self.arquivo = open(self.nome_arquivo, 'r')
+                linhas = self.arquivo.readlines()
                 total_de_linhas_atual = len(linhas)
 
                 if total_de_linhas != total_de_linhas_atual:
                     ultima_linha = linhas[total_de_linhas_atual - 1]
                     print ultima_linha
                 else:
-                    time.sleep(5)
+                    time.sleep(1)
 
 
 tail = Tail()
