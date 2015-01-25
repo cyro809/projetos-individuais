09e877a21df87b1d2b762b5b1fddcea4d415d339
cyro809 <cyro809@gmail.com>
2015-01-25 19:50:17 -0200

    Removendo testes desnecessarios

--- test_head.py
+++ test_head.py
@@ -4,32 +4,6 @@ import head
 
 class TestHeadClass(unittest.TestCase):
 
-    def test_le_comando_deve_atribuir_valores_corretos_aos_atributos_caso_nao_tenha_flags(self):
-        h = head.Head('head bla.txt')
-
-        tamanho_esperado = 1
-        nome_arquivo_esperado = 'bla.txt'
-        numero_de_linhas_esperada = 10
-
-        h.le_comando()
-
-        self.assertEqual(len(h.argumentos), tamanho_esperado)
-        self.assertEqual(h.num_de_linhas, numero_de_linhas_esperada)
-        self.assertEqual(h.nome_arquivo, nome_arquivo_esperado)
-
-    def test_le_comando_deve_atribuir_valores_corretos_aos_atributos_caso_tenha_flag_n(self):
-        h = head.Head('head -n 15 bla.txt')
-
-        tamanho_argumentos_esperado = 3
-        nome_arquivo_esperado = 'bla.txt'
-        numero_de_linhas_esperada = 15
-
-        h.le_comando()
-
-        self.assertEqual(len(h.argumentos), tamanho_argumentos_esperado)
-        self.assertEqual(h.num_de_linhas, numero_de_linhas_esperada)
-        self.assertEqual(h.nome_arquivo, nome_arquivo_esperado)
-
     def test_le_comando_deve_retornar_as_10_primeiras_linhas_por_default(self):
         h = head.Head('head bla.txt')
 
