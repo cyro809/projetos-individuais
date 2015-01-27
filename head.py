26aee30daf7c4333aab1827f90ba2dbdcb9138b4
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2015-01-27 14:06:24 -0200

    Removendo classe Head e transformando le_comando em função independente

--- head.py
+++ head.py
@@ -4,51 +4,48 @@
 import sys
 
 
-class Head(object):
+def le_comando(input_string):
+    num_de_linhas = 10
+    comando = input_string.split()[0]
 
-    def le_comando(self, input_string):
-        num_de_linhas = 10
-        comando = input_string.split()[0]
+    if comando != 'head':
+            return u'Comando Invalido'
+    elif comando == 'head' and len(input_string.split()) == 1:
+            return u'Especifique um Arquivo'
 
-        if comando != 'head':
-                return u'Comando Invalido'
-        elif comando == 'head' and len(input_string.split()) == 1:
-                return u'Especifique um Arquivo'
+    argumentos = input_string.split()[1:]
+    if len(argumentos) == 1:
+        nome_arquivo = argumentos[0]
 
-        argumentos = input_string.split()[1:]
-        if len(argumentos) == 1:
-            nome_arquivo = argumentos[0]
+    elif len(argumentos) == 2:
+        flag = argumentos[0]
 
-        elif len(argumentos) == 2:
-            flag = argumentos[0]
+        if flag == '-n':
+            nome_arquivo = argumentos[1]
+        else:
+            return u'Flag Desconhecida'
 
-            if flag == '-n':
-                nome_arquivo = argumentos[1]
-            else:
-                return u'Flag Desconhecida'
+    elif len(argumentos) == 3:
+        flag = argumentos[0]
 
-        elif len(argumentos) == 3:
-            flag = argumentos[0]
+        if flag == '-n':
+            try:
+                num_de_linhas = int(input_string.split()[2])
+                nome_arquivo = argumentos[2]
+            except ValueError:
+                return u'Parametro invalido. Especifique um numero'
+        else:
+            return u'Flag Desconhecida'
 
-            if flag == '-n':
-                try:
-                    num_de_linhas = int(input_string.split()[2])
-                    nome_arquivo = argumentos[2]
-                except ValueError:
-                    return u'Parametro invalido. Especifique um numero'
-            else:
-                return u'Flag Desconhecida'
+    try:
+        arquivo = open(nome_arquivo, 'r')
+    except IOError:
+        return 'Arquivo inexistente'
 
-        try:
-            arquivo = open(nome_arquivo, 'r')
-        except IOError:
-            return 'Arquivo inexistente'
-
-        return arquivo.readlines()[:num_de_linhas]
+    return arquivo.readlines()[:num_de_linhas]
 
 if __name__ == '__main__':
-    head = Head()
-    saida = head.le_comando(raw_input())
+    saida = le_comando(raw_input())
 
     for i in range(len(saida)):
         sys.stdout.write(saida[i])
--- test_head.py
+++ test_head.py
@@ -1,72 +1,62 @@
 import unittest
-import head
+from head import le_comando
 
 
 class TestHeadClass(unittest.TestCase):
 
     def test_le_comando_deve_retornar_as_10_primeiras_linhas_por_default(self):
-        h = head.Head()
 
-        self.assertEqual(h.le_comando('head bla.txt'), ['Baby I know,\n', "When we started out there were things you didn't know,\n",
-                                                        'But baby girl,\n', 'We got a lot of things we need to discuss\n',
-                                                        "I know I'm asking for allot, but just trust.\n",
-                                                        'You say that things getting old, sneaking round, creeping and love on the low\n',
-                                                        'But baby girl\n', "I can't wait till it's officially us,\n",
-                                                        "I can't wait to let them know about us.\n", '\n'])
+        self.assertEqual(le_comando('head bla.txt'), ['Baby I know,\n', "When we started out there were things you didn't know,\n",
+                                                      'But baby girl,\n', 'We got a lot of things we need to discuss\n',
+                                                      "I know I'm asking for allot, but just trust.\n",
+                                                      'You say that things getting old, sneaking round, creeping and love on the low\n',
+                                                      'But baby girl\n', "I can't wait till it's officially us,\n",
+                                                      "I can't wait to let them know about us.\n", '\n'])
 
     def test_deve_imprimir_as_8_primeiras_linhas_do_arquivo_bla_txt(self):
-        h = head.Head()
 
-        self.assertEqual(h.le_comando('head -n 8 bla.txt'), ['Baby I know,\n', "When we started out there were things you didn't know,\n",
-                                                             'But baby girl,\n', 'We got a lot of things we need to discuss\n',
-                                                             "I know I'm asking for allot, but just trust.\n",
-                                                             'You say that things getting old, sneaking round, creeping and love on the low\n',
-                                                             'But baby girl\n', "I can't wait till it's officially us,\n"])
-
-    def test_deve_retornar_arquivo_inexistente_caso_o_arquivo_nao_exista(self):
-        h = head.Head()
-
-        self.assertEqual(h.le_comando('head ble.txt'), 'Arquivo inexistente')
-
-    def test_deve_retornar_as_dez_primeiras_linhas_caso_passe_flag_n_sem_o_numero_de_linhas(self):
-        h = head.Head()
-
-        self.assertEqual(h.le_comando('head -n bla.txt'), ['Baby I know,\n', "When we started out there were things you didn't know,\n",
+        self.assertEqual(le_comando('head -n 8 bla.txt'), ['Baby I know,\n', "When we started out there were things you didn't know,\n",
                                                            'But baby girl,\n', 'We got a lot of things we need to discuss\n',
                                                            "I know I'm asking for allot, but just trust.\n",
                                                            'You say that things getting old, sneaking round, creeping and love on the low\n',
-                                                           'But baby girl\n', "I can't wait till it's officially us,\n",
-                                                           "I can't wait to let them know about us.\n", '\n'])
+                                                           'But baby girl\n', "I can't wait till it's officially us,\n"])
+
+    def test_deve_retornar_arquivo_inexistente_caso_o_arquivo_nao_exista(self):
+
+        self.assertEqual(le_comando('head ble.txt'), 'Arquivo inexistente')
+
+    def test_deve_retornar_as_dez_primeiras_linhas_caso_passe_flag_n_sem_o_numero_de_linhas(self):
+
+        self.assertEqual(le_comando('head -n bla.txt'), ['Baby I know,\n', "When we started out there were things you didn't know,\n",
+                                                         'But baby girl,\n', 'We got a lot of things we need to discuss\n',
+                                                         "I know I'm asking for allot, but just trust.\n",
+                                                         'You say that things getting old, sneaking round, creeping and love on the low\n',
+                                                         'But baby girl\n', "I can't wait till it's officially us,\n",
+                                                         "I can't wait to let them know about us.\n", '\n'])
 
     def test_deve_retornar_parametro_invalido_quando_passar_uma_letra_no_lugar_de_numero_para_quantidade_de_linhas_a_serem_impressas(self):
-        h = head.Head()
 
-        self.assertEqual(h.le_comando('head -n K bla.txt'), u'Parametro invalido. Especifique um numero')
+        self.assertEqual(le_comando('head -n K bla.txt'), u'Parametro invalido. Especifique um numero')
 
     def test_deve_retornar_especifique_um_arquivo_caso_o_comando_head_seja_passado_sem_argumentos(self):
-        h = head.Head()
 
-        self.assertEqual(h.le_comando('head'), u'Especifique um Arquivo')
+        self.assertEqual(le_comando('head'), u'Especifique um Arquivo')
 
     def test_deve_retornar_flag_inexistente_quando_passar_z_como_flag(self):
-        h = head.Head()
 
-        self.assertEqual(h.le_comando('head -z bla.txt'), u'Flag Desconhecida')
+        self.assertEqual(le_comando('head -z bla.txt'), u'Flag Desconhecida')
 
     def test_deve_retornar_parametro_invalido_caso_nao_passe_um_numero_no_lugar_da_flag(self):
-        h = head.Head()
 
-        self.assertEqual(h.le_comando('head 15 bla.txt'), u'Flag Desconhecida')
+        self.assertEqual(le_comando('head 15 bla.txt'), u'Flag Desconhecida')
 
     def test_deve_retornar_flag_desconhecida_ao_passar_z_como_flag_e_especificar_numero_de_linhas(self):
-        h = head.Head()
 
-        self.assertEqual(h.le_comando('head -z 15 bla.txt'), u'Flag Desconhecida')
+        self.assertEqual(le_comando('head -z 15 bla.txt'), u'Flag Desconhecida')
 
     def test_deve_retornar_comando_desconhecido_caso_nao_passe_head_como_comando(self):
-        h = head.Head()
 
-        self.assertEqual(h.le_comando('tail bla.txt'), u'Comando Invalido')
+        self.assertEqual(le_comando('tail bla.txt'), u'Comando Invalido')
 
 if __name__ == '__main__':
     unittest.main()
