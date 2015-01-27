ceb6d8a9ae0d1ee7799b5cb5dba0620fd06c905e
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2015-01-27 13:24:48 -0200

    Transformando atributos em variaveis locais do método le_comando

--- head.py
+++ head.py
@@ -8,51 +8,45 @@ class Head(object):
     def __init__(self, input_string):
         self.input_string = input_string
         self.comando = self.input_string.split()[0]
-        self.num_de_linhas = 10
-        self.flag = None
-        self.argumentos = []
-        self.arquivo = None
-        self.nome_arquivo = None
-        self.linhas_a_imprimir = None
-
 
     def le_comando(self):
+        num_de_linhas = 10
+
         if self.comando != 'head':
                 return u'Comando Invalido'
         elif self.comando == 'head' and len(self.input_string.split()) == 1:
                 return u'Especifique um Arquivo'
 
-        self.argumentos = self.input_string.split()[1:]
-        if len(self.argumentos) == 1:
-            self.nome_arquivo = self.argumentos[0]
+        argumentos = self.input_string.split()[1:]
+        if len(argumentos) == 1:
+            nome_arquivo = argumentos[0]
 
-        elif len(self.argumentos) == 2:
-            self.flag = self.argumentos[0]
+        elif len(argumentos) == 2:
+            flag = argumentos[0]
 
-            if self.flag == '-n':
-                self.nome_arquivo = self.argumentos[1]
+            if flag == '-n':
+                nome_arquivo = argumentos[1]
             else:
                 return u'Flag Desconhecida'
-            
-        elif len(self.argumentos) == 3:
-            self.flag = self.argumentos[0]
 
-            if self.flag == '-n':
+        elif len(argumentos) == 3:
+            flag = argumentos[0]
+
+            if flag == '-n':
                 try:
-                    self.num_de_linhas = int(self.input_string.split()[2])
-                    self.nome_arquivo = self.argumentos[2]
+                    num_de_linhas = int(self.input_string.split()[2])
+                    nome_arquivo = argumentos[2]
                 except ValueError:
                     return u'Parametro invalido. Especifique um numero'
             else:
                 return u'Flag Desconhecida'
 
         try:
-            self.arquivo = open(self.nome_arquivo, 'r')
+            arquivo = open(nome_arquivo, 'r')
         except IOError:
             return 'Arquivo inexistente'
-            
 
-        return self.arquivo.readlines()[:self.num_de_linhas]
+        return arquivo.readlines()[:num_de_linhas]
 
 if __name__ == '__main__':
     head = Head(raw_input())
