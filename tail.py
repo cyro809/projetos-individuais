0bbdf5bebe88ed0231352d782127827293aea53e
Cyro Guimaraes <cyro.guimaraes@corp.globo.com>
2015-01-30 18:21:37 -0200

    Removendo classe e deixando le_comando como função independente

--- tail.py
+++ tail.py
@@ -5,77 +5,74 @@ import sys
 import time
 
 
-class Tail(object):
+def le_comando(input_string):
+    comando = input_string.split()[0]
+    num_de_linhas = 10
+    flag = None
+    nome_arquivo = None
+    linhas_a_imprimir = None
 
-    def le_comando(self, input_string):
-        comando = input_string.split()[0]
-        num_de_linhas = 10
-        flag = None
-        nome_arquivo = None
-        linhas_a_imprimir = None
+    if comando == 'tail' and len(input_string.split()) == 1:
+        return 'Especifique um Arquivo'
+    elif comando != 'tail':
+        return 'Comando Invalido'
 
-        if comando == 'tail' and len(input_string.split()) == 1:
-            return 'Especifique um Arquivo'
-        elif comando != 'tail':
-            return 'Comando Invalido'
+    argumentos = input_string.split()[1:]
 
-        argumentos = input_string.split()[1:]
+    if len(argumentos) == 1:
+        nome_arquivo = argumentos[0]
 
-        if len(argumentos) == 1:
-            nome_arquivo = argumentos[0]
+    elif len(argumentos) == 2:
+        nome_arquivo = argumentos[1]
+        flag = argumentos[0]
 
-        elif len(argumentos) == 2:
-            nome_arquivo = argumentos[1]
-            flag = argumentos[0]
+        if flag != '-f':
+            return 'Flag Invalida'
 
-            if flag != '-f':
-                return 'Flag Invalida'
+    elif len(argumentos) == 3:
+        flag = argumentos[0]
 
-        elif len(argumentos) == 3:
-            flag = argumentos[0]
-
-            if flag == '-n':
-                try:
-                    num_de_linhas = int(input_string.split()[2])
-                except ValueError:
-                    return 'Parametro Invalido'
-                else:
-                    nome_arquivo = argumentos[2]
-
+        if flag == '-n':
+            try:
+                num_de_linhas = int(input_string.split()[2])
+            except ValueError:
+                return 'Parametro Invalido'
             else:
-                return u'flag não reconhecida!'
+                nome_arquivo = argumentos[2]
 
-        arquivo = open(nome_arquivo, 'r')
+        else:
+            return u'flag não reconhecida!'
 
-        linhas = arquivo.readlines()
-        total_de_linhas = len(linhas)
-        linha_limite = total_de_linhas - num_de_linhas
+    arquivo = open(nome_arquivo, 'r')
 
-        total_de_linhas_atual = total_de_linhas
-        if flag and flag == '-f':
-            contador = 0
-            while True:
-                total_de_linhas = total_de_linhas_atual
-                arquivo = open(nome_arquivo, 'r')
-                linhas = arquivo.readlines()
-                total_de_linhas_atual = len(linhas)
+    linhas = arquivo.readlines()
+    total_de_linhas = len(linhas)
+    linha_limite = total_de_linhas - num_de_linhas
 
-                if total_de_linhas != total_de_linhas_atual:
-                    ultima_linha = linhas[total_de_linhas_atual - 1]
+    total_de_linhas_atual = total_de_linhas
+    if flag and flag == '-f':
+        contador = 0
+        while True:
+            total_de_linhas = total_de_linhas_atual
+            arquivo = open(nome_arquivo, 'r')
+            linhas = arquivo.readlines()
+            total_de_linhas_atual = len(linhas)
 
-                    linhas_a_imprimir.append(ultima_linha)
-                    print ultima_linha
+            if total_de_linhas != total_de_linhas_atual:
+                ultima_linha = linhas[total_de_linhas_atual - 1]
 
-                    contador = 0
-                else:
-                    time.sleep(1)
-                    contador = contador + 1
+                linhas_a_imprimir.append(ultima_linha)
+                print ultima_linha
 
-                if contador >= 7:
-                    break
+                contador = 0
+            else:
+                time.sleep(1)
+                contador = contador + 1
 
-        return linhas[linha_limite:total_de_linhas]
+            if contador >= 7:
+                break
+
+    return linhas[linha_limite:total_de_linhas]
 
 if __name__ == '__main__':
-    tail = Tail()
-    tail.le_comando(raw_input())
+    le_comando(raw_input())
--- test_tail.py
+++ test_tail.py
@@ -1,11 +1,10 @@
 import unittest
-import tail
+from tail import le_comando
 
 
 class TestTailClass(unittest.TestCase):
 
     def test_deve_retornar_as_dez_ultimas_linhas_por_default(self):
-        t = tail.Tail()
 
         linhas_esperadas = ["Just a little bit, just a little bit, just a little bit, (baby I'm wait for you) longer, longer (won't you wait)\n",
                             'Just a little bit, just a little bit, just a little bit, longer, longer (if you wait)\n',
@@ -13,41 +12,35 @@ class TestTailClass(unittest.TestCase):
                             'Just a little bit\n', 'just a little bit\n', 'just a little bit\n', 'just a little bit\n',
                             'Just a little bit\n', 'Just a little bit\n']
 
-        self.assertEqual(t.le_comando('tail bla.txt'), linhas_esperadas)
+        self.assertEqual(le_comando('tail bla.txt'), linhas_esperadas)
 
     def test_deve_retornar_as_oito_ultimas_linhas_do_arquivo_bla_txt(self):
-        t = tail.Tail()
 
         linhas_esperadas = ['Just a little bit, just a little bit, just a little bit, longer.\n', '\n',
                             'Just a little bit\n', 'just a little bit\n', 'just a little bit\n', 'just a little bit\n',
                             'Just a little bit\n', 'Just a little bit\n']
 
-        self.assertEqual(t.le_comando('tail -n 8 bla.txt'), linhas_esperadas)
+        self.assertEqual(le_comando('tail -n 8 bla.txt'), linhas_esperadas)
 
     def test_deve_retornar_especifique_um_arquivo_quando_nao_passar_argumentos_para_tail(self):
-        t = tail.Tail()
 
-        self.assertEqual(t.le_comando('tail'), 'Especifique um Arquivo')
+        self.assertEqual(le_comando('tail'), 'Especifique um Arquivo')
 
     def test_deve_retornar_flag_invalida_caso_passe_z_como_flag(self):
-        t = tail.Tail()
 
-        self.assertEqual(t.le_comando('tail -z bla.txt'), 'Flag Invalida')
+        self.assertEqual(le_comando('tail -z bla.txt'), 'Flag Invalida')
 
     def test_deve_retornar_parametro_invalido_quando_passar_k_como_numero_de_linhas(self):
-        t = tail.Tail()
 
-        self.assertEqual(t.le_comando('tail -n k bla.txt'), 'Parametro Invalido')
+        self.assertEqual(le_comando('tail -n k bla.txt'), 'Parametro Invalido')
 
     def test_deve_retornar_flag_invalida_quando_passar_o_numero_de_linhas_sem_flags(self):
-        t = tail.Tail()
 
-        self.assertEqual(t.le_comando('tail 15 bla.txt'), 'Flag Invalida')
+        self.assertEqual(le_comando('tail 15 bla.txt'), 'Flag Invalida')
 
     def test_deve_retornar_comando_invalido_quando_passar_head_como_comando(self):
-        t = tail.Tail()
 
-        self.assertEqual(t.le_comando('head bla.txt'), 'Comando Invalido')
+        self.assertEqual(le_comando('head bla.txt'), 'Comando Invalido')
 
 if __name__ == '__main__':
     unittest.main()
